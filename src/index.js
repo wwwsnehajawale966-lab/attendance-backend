744a16da1979f43ae30b985b1dcb07e2e1f27d64
Sneha Jawale <snehajawale966@gmail.com>
2026-06-05 14:27:47 +0530

    Fix Render port binding issue by listening on 0.0.0.0

--- src/index.js
+++ src/index.js
@@ -55,6 +55,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
